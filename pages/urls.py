56ae71bebf1b7997ff95d316abeb3e46e15059ae
Dieguidev <diegogaraycullas@gmail.com>
2023-12-21 22:13:08 -0500

    page_list.com a pge_detail.html y configurada la vista de PageDetailsView

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from .views import *
 
-from . import views
 urlpatterns = [
     path('', PageListView.as_view(), name='pages'),
-    path('<int:page_id>/<slug:page_slug>/', views.page, name='page'),
+    path('<int:pk>/<slug:page_slug>/', PageDetailView.as_view(), name='page'),
 ]
