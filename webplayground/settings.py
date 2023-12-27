25ac7329ae6150571c06fbd0e888eb8834c8fee3
Dieguidev <diegogaraycullas@gmail.com>
2023-12-27 11:29:19 -0500

    registration app: creada vista url y template para el perfil

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 #* auth redirects
-LOGIN_REDIRECT_URL = "pages:pages"
+# LOGIN_REDIRECT_URL = "pages:pages"
 LOGOUT_REDIRECT_URL = "home"
 
 import os
