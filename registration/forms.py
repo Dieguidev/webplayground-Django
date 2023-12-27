42b5af9f1aef46c12a3738e500bbd553ed57b024
Dieguidev <diegogaraycullas@gmail.com>
2023-12-27 16:01:49 -0500

    registration app: creada la vista url y template para cambiar de email

--- registration/forms.py
+++ registration/forms.py
@@ -29,3 +29,17 @@ class ProfileForm(forms.ModelForm):
             'bio': forms.Textarea(attrs={'class':'form-control mt-3', 'rows':3, 'placeholder':'Biografía'}),
             'link': forms.URLInput(attrs={'class':'form-control mt-3', 'placeholder':'Link de perfil'}),
         }
+        
+class EmailForm(forms.ModelForm):
+    email = forms.EmailField(required=True, help_text="Requerido. 254 caracteres como máximo y debe ser valido")
+
+    class Meta:
+        model = User
+        fields = ["email"]
+    #comprobando que el email no exista    
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if 'email' in self.changed_data:
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError("El email ya existe")
+        return email
--- registration/urls.py
+++ registration/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('profile/', ProfileUpdate.as_view(), name='profile'),
+    path('profile/email/', EmailUpdate.as_view(), name='profile_email'),
 ]
