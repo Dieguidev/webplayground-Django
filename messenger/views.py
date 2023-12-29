d476e193e4ba65c6d12dbe27efcb947dc4989064
Dieguidev <diegogaraycullas@gmail.com>
2023-12-29 12:23:43 -0500

    messenger app

--- messenger/views.py
+++ messenger/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView 
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
+from .models import Thread
+from django.http import Http404
+
+
 
 # Create your views here.
+
+#esta vista es para que el usuario no pueda acceder a la vista de los mensajes si no esta logeado y además debido a la relacion con users filtra solo sus threads
+@method_decorator(login_required, name="dispatch")
+class ThreadList(TemplateView):
+    template_name = "messenger/thread_list.html"
+
+
+@method_decorator(login_required, name="dispatch")
+class ThreadDetail(DetailView):
+    model = Thread
+    
+    def get_object(self):
+        obj = super(ThreadDetail, self).get_object()
+        if self.request.user not in obj.users.all():
+            raise Http404()
+        return obj
